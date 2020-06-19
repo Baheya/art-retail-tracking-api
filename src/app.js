30bbc8f34986b1a26057360fbabd333d6300e1d4
Baheya <khalifa.baheya@gmail.com>
2020-06-19 09:41:35 +0400

    refactored code

---
+++ src/app.js
@@ -0,0 +1,34 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+require('../db/mongoose');
+
+const artworkRoutes = require('../routes/artwork');
+const userRoutes = require('../routes/user');
+const { handleError } = require('../helpers/error');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+app.use(artworkRoutes);
+app.use(userRoutes);
+
+app.use((err, req, res, next) => {
+  if (err.statusCode === undefined) {
+    err.statusCode = 500;
+  }
+  handleError(err, res);
+});
+
+module.exports = app;
