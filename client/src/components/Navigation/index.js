f7baa33b2fd8552cf5d5f922286d51c43c24a1af
Baheya Khalifa <baheya@riseup.co>
2020-06-24 16:08:29 +0400

    added active link styling to navigation

--- client/src/components/Navigation/index.js
+++ client/src/components/Navigation/index.js
@@ -1,23 +1,23 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import '../../styles/navigation.scss';
 
 const Navigation = () => {
   return (
     <nav>
       <ul>
-        <Link to="/home">
+        <NavLink activeClassName="active" to="/home">
           <li>Home</li>
-        </Link>
-        <Link to="/upload">
+        </NavLink>
+        <NavLink activeClassName="active" to="/upload">
           <li>Upload</li>
-        </Link>
-        <Link to="/sales">
+        </NavLink>
+        <NavLink activeClassName="active" to="/sales">
           <li>Sales</li>
-        </Link>
-        <Link to="/profile">
+        </NavLink>
+        <NavLink activeClassName="active" to="/profile">
           <li>Profile</li>
-        </Link>
+        </NavLink>
       </ul>
     </nav>
   );
