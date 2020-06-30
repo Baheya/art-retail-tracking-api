50da9b6ceae8eb7252be99eaf0cf314ee53e35cc
Baheya Khalifa <baheya@riseup.co>
2020-06-30 20:30:37 +0400

    updated artworks page styling and added illustration

---
+++ client/src/components/Illustrations/ArtworksIllustration.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import artworkIllustration from '../../assets/Art-lover-pana.svg';
+
+import '../../styles/artworksillustration.scss';
+
+const ArtworksIllustration = () => {
+  return (
+    <img className="artworks__illustration" src={artworkIllustration} alt="" />
+  );
+};
+
+export default ArtworksIllustration;
