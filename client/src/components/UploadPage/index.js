0c80a721b0e9f00efdb365d36516eee3c751d94d
Baheya <khalifa.baheya@gmail.com>
2020-08-04 11:54:56 +0400

    updated upload page styling

--- client/src/components/UploadPage/index.js
+++ client/src/components/UploadPage/index.js
@@ -1,28 +1,38 @@
 import React, { useState } from 'react';
-import axios from 'axios';
-
 import { useAuth } from '../../context/auth';
-import Input from '../Input/index';
-import Button from '../Button/index';
-import UploadIllustration from '../Illustrations/UploadIllustration';
 import { Redirect } from 'react-router-dom';
+import axios from 'axios';
+import FadeIn from 'react-fade-in';
 
-import '../../styles/uploadpage.scss';
+import ArtworkForm from '../ArtworkForm';
+
+import './uploadpage.scss';
+import Loading from '../Loading';
 
 const UploadPage = () => {
-  const [title, setTitle] = useState('');
-  const [image, setImage] = useState('');
+  const [artworkForm, setArtworkForm] = useState({});
   const [errorMessage, setErrorMessage] = useState('');
   const [isError, setIsError] = useState(false);
   const [isUploaded, setIsUploaded] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const { authTokens } = useAuth();
+
+  const updateForm = (value, name) => {
+    setArtworkForm((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+    console.log(artworkForm);
+  };
 
   const postSubmit = async (e) => {
     e.preventDefault();
+    setIsLoading(true);
     try {
       const formData = new FormData();
-      formData.append('title', title);
-      formData.append('image', image);
+      Object.keys(artworkForm).map((key) => {
+        formData.append(`${key}`, artworkForm[key]);
+      });
       const response = await axios.post('/api/upload-artwork', formData, {
         headers: {
           Authorization: `Bearer ${authTokens}`,
@@ -30,9 +40,11 @@ const UploadPage = () => {
       });
       if (response.status === 201) {
         setIsUploaded(true);
+        setIsLoading(false);
       }
     } catch (error) {
       setIsError(true);
+      setIsLoading(false);
       setErrorMessage(error.response.data.message);
     }
   };
@@ -42,43 +54,21 @@ const UploadPage = () => {
   }
 
   return (
-    <div className="upload__container">
-      <h2>Upload Artwork</h2>
-      <div className="upload__form__container">
-        <form className="upload__form">
-          <label htmlFor="title">Title: </label>
-          <Input
-            type="text"
-            id="title"
-            name="title"
-            placeholder="Enter title here"
-            onChange={(e) => setTitle(e.target.value)}
-            value={title}
-          />
-          <label htmlFor="image" className="custom__file__input">
-            <Input
-              type="file"
-              id="image"
-              name="image"
-              onChange={(e) => setImage(e.target.files[0])}
-            />
-            Select a file
-          </label>
-          <input
-            type="text"
-            className="image__selection__text__preview"
-            value={image ? image.name : 'No file chosen'}
-          />
-          <Button
-            className="artwork__upload__button"
-            onClick={postSubmit}
-            displayText="Upload"
+    <FadeIn>
+      {isLoading ? (
+        <Loading text="Uploading your art" />
+      ) : (
+        <div className="upload_page__container">
+          <ArtworkForm
+            updateForm={updateForm}
+            postSubmit={postSubmit}
+            artworkForm={artworkForm}
+            buttonText="Upload"
           />
           {isError && <p className="upload__error__message">{errorMessage}</p>}
-        </form>
-      </div>
-      <UploadIllustration />
-    </div>
+        </div>
+      )}
+    </FadeIn>
   );
 };
 
