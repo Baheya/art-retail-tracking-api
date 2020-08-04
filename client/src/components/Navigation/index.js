1f9bf561ad84df0b3d1ae266c45094b42fc9538e
Baheya Khalifa <baheya@riseup.co>
2020-08-04 11:52:33 +0400

    updated navigation styling

--- client/src/components/Navigation/index.js
+++ client/src/components/Navigation/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import '../../styles/navigation.scss';
+import './navigation.scss';
 
 const Navigation = () => {
   return (
