2cab5883d4b3951272d24b666820c13deec27e7a
Baheya Khalifa <baheya@riseup.co>
2020-08-04 15:51:44 +0400

    fixed bug

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
 const app = require('./app');
-const path = require('path');
 const port = process.env.PORT || 8080;
-
-app.use(express.static(path.join(__dirname, 'client', 'build')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-});
 
 app.listen(port, () => {
   console.log('Hi from port ' + port + '!');
