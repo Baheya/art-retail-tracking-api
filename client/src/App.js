265a292ac695d1d86f77d26afe199fd80fea6b33
Baheya <khalifa.baheya@gmail.com>
2020-08-04 11:44:38 +0400

    split components and imported them lazily for better performance

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,25 @@
-import React, { useState } from 'react';
-import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
-import './App.css';
+import React, { useState, lazy, Suspense } from 'react';
+import {
+  Switch,
+  Route,
+  BrowserRouter as Router,
+  Redirect,
+} from 'react-router-dom';
 import { AuthContext } from './context/auth';
+
+import './sass/main.scss';
+
 import Header from './components/Header';
-import Divider from './components/Divider';
-import LoginComponent from './components/LoginComponent';
-import RegisterComponent from './components/RegisterComponent';
-import Artworks from './components/Artworks';
-import UploadPage from './components/UploadPage';
-import PrivateRoute from './components/PrivateRoute';
-import Artwork from './components/Artwork';
+import Loading from './components/Loading';
+
+const LoginComponent = lazy(() => import('./components/LoginComponent'));
+const RegisterComponent = lazy(() => import('./components/RegisterComponent'));
+const Artworks = lazy(() => import('./components/Artworks'));
+const UploadPage = lazy(() => import('./components/UploadPage'));
+const PrivateRoute = lazy(() => import('./components/PrivateRoute'));
+const Artwork = lazy(() => import('./components/Artwork'));
+const Sales = lazy(() => import('./components/Sales'));
+const Profile = lazy(() => import('./components/Profile'));
 
 const App = () => {
   const existingTokens = JSON.parse(localStorage.getItem('tokens'));
@@ -24,14 +34,17 @@ const App = () => {
     <AuthContext.Provider value={{ authTokens, setAuthTokens: setTokens }}>
       <Router>
         <Header />
-        <Divider />
-        <Switch>
-          <Route exact path="/login" component={LoginComponent} />
-          <Route path="/register" component={RegisterComponent} />
-          <PrivateRoute path="/home" component={Artworks} />
-          <PrivateRoute path="/upload" component={UploadPage} />
-          <PrivateRoute path="/artworks/:id" component={Artwork} />
-        </Switch>
+        <Suspense fallback={<Loading text="Fetching Data" />}>
+          <Switch>
+            <Route exact path="/login" component={LoginComponent} />
+            <Route path="/register" component={RegisterComponent} />
+            <PrivateRoute path="/home" component={Artworks} />
+            <PrivateRoute path="/upload" component={UploadPage} />
+            <PrivateRoute path="/artworks/:id" component={Artwork} />
+            <PrivateRoute path="/sales" component={Sales} />
+            <PrivateRoute path="/profile" component={Profile} />
+          </Switch>
+        </Suspense>
       </Router>
     </AuthContext.Provider>
   );
