0c65d586f6072fa6d8806f76f3a4d3e1fd5c9c64
Baheya <khalifa.baheya@gmail.com>
2020-08-20 14:41:06 +0400

    moved compression middleware up in middleware chain

--- src/app.js
+++ src/app.js
@@ -13,10 +13,10 @@ const { handleError } = require('../helpers/error');
 
 const app = express();
 
+app.use(compression());
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
-app.use(compression());
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
